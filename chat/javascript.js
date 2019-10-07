4116ddee0bf583ee58b6335655910c2bcd0f93a2
Alan Chmielecki <55060456+BlekDimon@users.noreply.github.com>
2019-10-07 13:15:42 +0200

    Update javascript.js

--- chat/javascript.js
+++ chat/javascript.js
@@ -1,5 +1,11 @@
 $("#input").on('keyup', function(event) {
-	if (event.keyCode === 13 && document.getElementById("input").value.length != 0) {
+	var check = false;
+        for (var i=0; i<document.getElementById("input").value.length; i++) {
+                if (document.getElementById("input").value[i] != " ") {
+                        check = true;
+                }
+        }
+	if (event.keyCode === 13 && document.getElementById("input").value.length != 0 && check) {
 		var node = document.createElement("div");
 		var textNode = document.createTextNode("Gracz: " + document.getElementById("input").value);
 		node.appendChild(textNode);
